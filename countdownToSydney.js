3e27837fa2e6339c83360b17c2596b36c2677e79
Orlando <oobo@gmx.com>
2015-05-22 22:00:59 -0400

    countdown update

--- countdownToSydney.js
+++ countdownToSydney.js
@@ -1,1 +1,14 @@
-var DateNow = new Date()
+var DateNow = new Date();
+var DateFlight = new Date(2015,5,16,15);
+
+function calcDaysRemain(date1, date2, display) {
+    var diffDates = (date2 - date1)/(1000*3600*24);
+    
+    if (display == "alert") {
+        alert("days remaining til flight: " + Math.floor(diffDates));
+    }
+    else (console.log(diffDates));
+
+}
+
+calcDaysRemain(DateNow, DateFlight, "alert");
